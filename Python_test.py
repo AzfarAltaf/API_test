c4e8fb60db5f0ab8df7a3820922693f8fa69dd40
AzfarAltaf <azfarrindbaloch@gmail.com>
2022-09-05 16:37:56 +0500

    Second Commit

--- Python_test.py
+++ Python_test.py
@@ -1,36 +1,55 @@
 from flask import Flask, request
+import time
+import mysql.connector as database
+
+# Connecting to the database
+connection = database.connect(
+        user='root',
+        password='password',
+        host=192.168.1.28,
+        database=flask_database)
+cursor = connection.cursor()
+
+# Function for Writing database
+def write_data_sorting(ip,data,result,status,oper,time):
+    stmt_insert = "INSERT INTO Sorting ("+ip+","+data+","+result+","+status+","+oper+","+time+")"
+    cursor.execute(stmt_insert)
+    print("Logs Updated Successfully")
+
 
 
 # Function for sorting in ascending order
 def sort_asc(array):
-    print(array)
     array.sort()
-    print(array)
     return array
-
+# Function for sorting in descending order
 def sort_des(array):
     array.sort(reverse=True)
     return array
 
-
 app = Flask(__name__)
-@app.route('/sorting/launch', methods=['POST'])
+@app.route('/sorting', methods=['POST'])
 def launch():
     arr= None
     method = None
+    ipAddr = request.remote_add
     data = request.get_json()
+    req_time = time.time()
     print(data)
     arr = data["array"]
     print(arr)
     method = data["method"]
     if method == "Ascending":
         sortArray = sort_asc(arr)
-        result = {"Status" : "Done", "Sorted Array": sortArray}
+        stat = "DONE"
     elif method == "Decending":
         sortArray = sort_des(arr)
-        result = {"Status" : "Done", "Sorted Array": sortArray}
+        stat = "DONE"
     else:
-        result = {"Status": "Failed", "Feedback": "Please provide correct method"}
+        sortArray = 'Please provide the correct operation'
+        stat = "FAILED"
+    result = {"Status" : stat, "Sorted Array": sortArray}
+    write_data_sorting(ipAddr,str(arr),str(sortArray),stat,method,req_time)
     print(result)
     return result
 
